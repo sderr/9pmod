905c27329b6e0d5a7d9c959ed9f259c49e4f9d65
Simon Derr <simon.derr@bull.net>
2013-07-23 13:49:59 +0200

    9pnet: Adding stats on duration and number of active requests

--- net/9p/stats.c
+++ net/9p/stats.c
@@ -13,25 +13,66 @@
 #include "protocol.h"
 #include "stats.h"
 
+/* We build two histograms:
+ * - duration of 9p requests. This one has a logarithmic scale.
+ * - number of active requests. This one has a linear scale.
+ */
 
-
+#define TSLOTS 10 /* slots for the time histogram */
+#define TSHIFT 8  /* bit shift for time histogram i.e. size of first bucket */
+#define ASLOTS 12 /* slots for the active requests histogram */
+#define ASTEP 3   /* size of each bucket */
+ 
 DEFINE_PER_CPU(unsigned long, reqtime);
 DEFINE_PER_CPU(unsigned long, nr_req);
+DEFINE_PER_CPU(unsigned long[TSLOTS], time_hist);
+DEFINE_PER_CPU(unsigned long[ASLOTS], ar_hist);
+
+atomic_t active_requests;
 
 static struct proc_dir_entry *dir;
 
-void p9stat_record(long delta_us)
+void p9stat_record(long delta_us, int active_req)
 {
+	int t_slot;
+	int a_slot;
+	
 	get_cpu_var(reqtime) += delta_us;
 	put_cpu_var(reqtime);
 	get_cpu_var(nr_req)++;
 	put_cpu_var(nr_req);
+
+	t_slot = max(0, min_t(int, TSLOTS-1, fls(delta_us) - TSHIFT));
+	a_slot = min(ASLOTS-1, active_req/ASTEP);
+
+	get_cpu_var(time_hist)[t_slot]++;
+	put_cpu_var(time_hist);
+
+	get_cpu_var(ar_hist)[a_slot]++;
+	put_cpu_var(ar_hist);
 }
 
+
+void p9stat_enter(ktime_t *t)
+{
+	*t = ktime_get();
+	atomic_inc(&active_requests);
+}
+
+void p9stat_leave(ktime_t *t)
+{
+	int ar = atomic_read(&active_requests);
+	ktime_t end = ktime_get();
+	s64 delta = ktime_us_delta(end, *t);
+
+	atomic_dec(&active_requests);
+	p9stat_record(delta, ar);
+}
 
 static int stats_show(struct seq_file *m, void *v)
 {
 	int j;
+	int i;
 	unsigned long total_reqtime = 0;
 	unsigned long total_nr_req = 0;
 
@@ -40,8 +81,38 @@ static int stats_show(struct seq_file *m, void *v)
 		total_nr_req += per_cpu(nr_req, j);
 	}
 
-	seq_printf(m, "Requests: %lu\n", total_nr_req);
-	seq_printf(m, "Time (us): %lu\n", total_reqtime);
+	seq_printf(m, "Total requests: %lu\n", total_nr_req);
+	seq_printf(m, "Total time (us): %lu\n", total_reqtime);
+	seq_printf(m, "Request duration (us):\n");
+
+	for(i = 0; i < TSLOTS; i++) {
+		int total = 0;
+		for_each_online_cpu(j)
+			total += per_cpu(time_hist, j)[i];
+		if (i < TSLOTS - 1)
+			seq_printf(m, "     %5d - %5d us : %d\n",
+			           i ? 1<<(i+TSHIFT-1) : 0, 
+				   (1<<(i+TSHIFT))-1, total);
+		else
+			seq_printf(m, "     >= %5d us      : %d\n",
+			           1<<(i+TSHIFT-1), total);
+
+	}
+
+	seq_printf(m, "Active requests:\n");
+	for(i = 0; i < ASLOTS; i++) {
+		int total = 0;
+		for_each_online_cpu(j)
+			total += per_cpu(ar_hist, j)[i];
+		if (i < ASLOTS - 1)
+			seq_printf(m, "     %3d - %3d : %d\n",
+			           i*ASTEP, (i+1) * ASTEP - 1, total);
+		else
+			seq_printf(m, "     >= %3d    : %d\n",
+			           i*ASTEP, total);
+	}
+
+
 	return 0;
 }
 
@@ -59,7 +130,7 @@ static const struct file_operations stats_fops = {
 
 void stats_init(void)
 {
-	/* create /proc/net/9p */
+	/* create /proc/fs/9p */
 	dir = proc_mkdir("fs/9p", NULL);
 	if (!dir) {
 		printk("Could not create 9p directory.\n");
--- net/9p/stats.h
+++ net/9p/stats.h
@@ -1,19 +1,7 @@
 
 
-void p9stat_record(long delta_us);
-
-static inline void p9stat_enter(ktime_t *t)
-{
-	*t = ktime_get();
-}
-
-static inline void p9stat_leave(ktime_t *t)
-{
-	ktime_t end = ktime_get();
-	s64 delta = ktime_us_delta(end, *t);
-
-	p9stat_record(delta);
-}
+void p9stat_enter(ktime_t *t);
+void p9stat_leave(ktime_t *t);
 
 void stats_init(void);
 void stats_exit(void);
