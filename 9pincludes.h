250e0a1f65bc697ad1e70bc46d473d4ab781847e
Simon Derr <simon.derr@bull.net>
2015-01-28 14:25:42 +0100

    Add 9p CONFIG options

--- 9pincludes.h
+++ 9pincludes.h
@@ -4,3 +4,8 @@
 #include <linux/sched.h>
 #include <linux/idr.h>
 #include <linux/slab.h>
+
+//#define CONFIG_9P_FSCACHE 1
+#define CONFIG_9P_FS_POSIX_ACL 1
+//#define CONFIG_9P_FS_SECURITY 1
+#define CONFIG_NET_9P_DEBUG 1
